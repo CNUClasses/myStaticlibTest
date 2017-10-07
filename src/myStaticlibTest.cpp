a525bf6416b46b1e5e369c53388e28cf32029fe2
Keith <kperkins411@gmail.com>
2017-10-06 23:04:38 -0400

    goes with myStaticlib project

---
+++ src/myStaticlibTest.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : myStaticlibTest.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+#include "../myStaticlib/includes/myStaticLib2.h"
+
+int main() {
+	show();
+}
